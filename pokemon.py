2ef60a6aed1f21197fc0cb3ca9fe3b4486298ec5
DeeJay <dylansumser@googlemail.com>
2020-10-23 12:31:28 +0200

    Added loose health and knock out methods to Pokemon class

--- pokemon.py
+++ pokemon.py
@@ -12,5 +12,35 @@ class Pokemon:
         self.current_health = level
         self.knocked_out = False
 
+    def knock_out(self):
+
+        self.knocked_out = True
+        
+    def loose_health(self, value):
+
+        if value < self.current_health:
+            self.current_health -= value
+
+        else:
+            self.current_health = 0
+            self.knock_out()
+
+
+        
+
+        
 
 test = Pokemon("charizard", 50, "fire")
+
+test.loose_health(10)
+
+print(test.current_health)
+
+test.loose_health(40)
+
+print(test.current_health)
+print(test.knocked_out)
+
+
+
+
